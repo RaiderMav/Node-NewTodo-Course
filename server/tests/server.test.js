167168804d7c82d65a741f8113b13928a36d6d59
RaiderMav <raidermaverick@gmail.com>
2017-10-10 17:59:43 -0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -224,3 +224,25 @@ describe(`POST /users`, () => {
     .end(done)
   })
 })
+
+describe('DELETE /users/me/token', () => {
+  it(`should remove auth token on log out`, (done) => {
+    // Delete /users/me/token
+    // Set x-auth equal to token
+    // expect 200
+    // find user and verify token array has length zero
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        return done(err)
+      }
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens.length).toBe(0)
+        done()
+      }).catch((e) => done(e))
+    })
+  })
+})
